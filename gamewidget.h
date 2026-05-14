997986d63facf0b0876f76a19bbba910e4866ea1
boredwind777 <507661035@qq.com>
2026-05-14 13:40:51 +0800

    第二版所有文件

---
+++ gamewidget.h
@@ -0,0 +1,201 @@
+#ifndef GAMEWIDGET_H
+#define GAMEWIDGET_H
+
+#include <QWidget>
+#include <QKeyEvent>
+#include <QTimer>
+#include <QPainter>
+#include <QTime>
+#include <QVector>
+#include <QPolygon>
+#include <QRectF>
+
+// ===================== 游戏常量 =====================
+#define PLAYER_SIZE 30
+#define MOVE_SPEED 240.0
+#define JUMP_POWER -400.0
+#define GRAVITY 1200.0
+#define SPIKE_SIZE 20
+#define GAME_WIDTH 1200
+#define GAME_HEIGHT 800
+#define MAP_WIDTH 1500
+
+// ===================== 行为组件 =====================
+struct MoveHoriz {
+    int left, right;
+    double speed;
+    int dir;
+    double deltaX;
+
+    MoveHoriz(int l, int r, double sp);
+    void update(double &x, double w, double dt);
+};
+
+struct MoveVert {
+    int top, bottom;
+    double speed;
+    int dir;
+
+    MoveVert(int t, int b, double sp);
+    void update(double &y, double h, double dt);
+};
+
+struct Disappear {
+    bool visible;
+    Disappear(bool initVis);
+};
+
+
+
+// ===================== 游戏物体基类 =====================
+class GameObject
+{
+public:
+    double x, y, w, h;
+
+    MoveHoriz* moveHoriz = nullptr;
+    MoveVert* moveVert = nullptr;
+    Disappear* disappear = nullptr;
+
+    bool isTrap = false;
+    QRectF triggerArea;
+    bool triggered = false;
+    double trapKeepTime = 3.0;
+    double trapTimer = 0.0;
+    double triggerTimer = 0.0;
+    double needStaySec = 1.0;
+    bool oneShot = true;
+    bool trapdestroyed = false;
+
+    bool isFallTrap = false;
+    bool fallTriggered = false;
+    double fallTimer = 0.0;
+    double fallRecoverTime = 2.5;
+
+    bool customHideMode = false;
+    double showTime = 2.0;
+    double hideTime = 2.0;
+    double customTimer = 0.0;
+
+    bool hiddenBoard = false;
+    bool boardRevealed = false;
+
+    bool isFlySpike = false;
+    int flyDir = 1;
+    double flySpeed = 350;
+    bool flyBack = false;
+    double flyBackInterval = 1.5;
+
+    QRectF flyTriggerRange;
+    bool flyDone = false;
+    bool flylaunched = false;
+    bool isLocked = false;
+    double flyLeftBound = 0.0;
+    double flyRightBound = MAP_WIDTH;
+
+public:
+    GameObject(double x_, double y_, double w_, double h_);
+    virtual ~GameObject() = default;
+
+    virtual void update(double dt) final;
+    virtual void onUpdate(double dt);
+    virtual void draw(QPainter &p, double camerax) = 0;
+
+    QRectF rect() const;
+    bool isVisible() const;
+};
+
+class Platform;
+class Spike;
+
+// ===================== 玩家类 =====================
+class GameWidget;
+class Player
+{
+public:
+    bool isDying = false;
+private:
+    double x, y, vx, vy;
+    double platformspeedx;
+    int jumpCount;
+    const int maxJump = 2;
+    bool jumppressedlast;
+    bool onGround;
+
+public:
+    void init();
+    void move(double dt);
+    void draw(QPainter &p, double camerax);
+    void input(bool A, bool D, bool W);
+    QRectF rect() const;
+     void respawn();
+    bool checkCollisions(QVector<GameObject*>& objs, double dt);
+
+    double getx() const;
+    double gety() const;
+    double setx(double a){x=a;}
+        double sety(double b){y=b;}
+    double setvx(double a);
+    double setvy(double b);
+    double getvx();
+    double getvy();
+    bool getonground();
+    void setOnGround(bool on) {
+            onGround = on;
+        }
+        void setJumpCount(int count) {
+            jumpCount = count;
+        }
+};
+
+// ===================== 游戏窗口 =====================
+class GameWidget : public QWidget
+{
+    Q_OBJECT
+private:
+    Player player;
+    QTimer timer;
+    QTime lastTime;
+    bool keyA, keyD, keyW;
+    QVector<GameObject*> objs;
+    double camerax;
+    bool isDying = false;
+    double dyingTimer = 0.0;
+    const double DYING_DELAY = 0.6;
+    int deathCount = 0;
+    bool isWin = false;
+    bool showTip = false;
+    double tipX=0, tipY=0;
+    bool isMenu = false;
+    bool isPause = false;
+    int pauseSel = 0;
+    bool isConfirmStart = true;
+    bool isQuitSad = false;
+
+public:
+    double saveX = 100;        // 存档X坐标（默认出生点）
+    double saveY = GAME_HEIGHT - PLAYER_SIZE;  // 存档Y坐标
+    bool hasSavePoint = false; // 是否激活过存档
+
+    explicit GameWidget(QWidget *p = nullptr);
+    ~GameWidget() override;
+    void resetAllTraps();
+private:
+    void initGame();
+    void drawGame(QPainter &p);
+
+protected:
+    void paintEvent(QPaintEvent*) override;
+    void keyPressEvent(QKeyEvent*) override;
+    void keyReleaseEvent(QKeyEvent*) override;
+};
+class SavePoint : public GameObject
+{
+public:
+    using GameObject::GameObject;
+    void draw(QPainter &p, double camerax) override;
+     bool saved = false;
+};
+
+
+#endif
---
+++ menuwindow.cpp
@@ -0,0 +1,113 @@
+#include "menuwindow.h"
+#include "gamewidget.h"
+#include <QFont>
+#include <QKeyEvent>
+
+MenuWindow::MenuWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setFixedSize(1200, 800);
+    setWindowTitle("I Wanna 自制版 - 狱门疆领域");
+
+    storyLine = 0;
+    storyFinished = false;
+    storyTexts << "【旁白】狱门疆之内，时空凝滞，无边死寂。"
+              << "【咒灵】终于……等到你了。"
+              << "【五条悟】哦？没想到这种地方，还藏着这么有意思的家伙。"
+              << "【咒灵】吾乃由无数人沉沦I wanna所积攒的怨念化生之特技咒灵。"
+              << "【咒灵】自我被封入狱门疆的那一刻，我便已展开领域。"
+              << "【咒灵】借狱门疆隔绝外界的特性，沉眠至今，静候有人踏入。"
+              << "【咒灵】来一场游戏吧。"
+              << "【咒灵】束缚已然定下：你在此领域内封禁全部咒术与咒力。"
+              << "【咒灵】而我，不得插手游戏分毫。"
+              << "【咒灵】若你能抵达领域终点，便可借我曾经收集的天逆鉾之力冲破狱门疆。"
+              << "【咒灵】若你不能……呵呵，那就献上一切换取我的自由！"
+              << "【五条悟】有点意思啊……既然都找上门了，那我就陪你玩玩好了。"
+              << "【系统】游戏开启，前路皆由脚下而行……";
+
+    storyTimer = new QTimer(this);
+    connect(storyTimer, &QTimer::timeout, this, &MenuWindow::updateStory);
+    storyTimer->start(1500);
+
+    // ========== 只创建一次按钮！！！ ==========
+    btnStart = new QPushButton("踏入领域 · 奔赴狱门", this);
+    btnStart->setFixedSize(260, 70);
+    btnStart->hide();
+
+    btnStart->setStyleSheet(R"(
+        QPushButton{
+            font-size:22px;
+            font-weight:bold;
+            background-color:#2b0808;
+            color:#bb2222;
+            border:3px solid #661111;
+            border-radius:8px;
+        }
+        QPushButton:hover{
+            background-color:#440c0c;
+            color:#ff4444;
+            border:3px solid #aa2222;
+        }
+        QPushButton:pressed{
+            background-color:#1a0505;
+            color:#ff6666;
+        }
+    )");
+
+    connect(btnStart, &QPushButton::clicked, this, &MenuWindow::onStartGameClicked);
+}
+
+void MenuWindow::updateStory()
+{
+    if (storyFinished) return;
+
+    storyLine++;
+    if (storyLine >= storyTexts.size()) {
+        storyTimer->stop();
+        storyFinished = true;
+
+        // ========== 按钮位置，改这里就动！ ==========
+        btnStart->move(470, 680);
+        btnStart->show();
+    }
+    update();
+}
+
+void MenuWindow::paintEvent(QPaintEvent *event)
+{
+    QMainWindow::paintEvent(event);
+    QPainter p(this);
+    p.fillRect(rect(), QColor(5, 5, 18));
+    p.setPen(QColor(200,200,220));
+    p.setFont(QFont("Microsoft YaHei", 14));
+
+    int y = 100;
+    for (int i = 0; i <= storyLine && i < storyTexts.size(); i++) {
+        p.drawText(70, y, storyTexts[i]);
+        y += 36;
+    }
+
+    p.setPen(QColor(100,100,120));
+    p.drawText(width() - 240, height() - 30, "按 ESC 跳过剧情");
+}
+
+void MenuWindow::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key() == Qt::Key_Escape) {
+        storyTimer->stop();
+        storyFinished = true;
+        storyLine = storyTexts.size() - 1;
+        btnStart->move(470, 680);
+        btnStart->show();
+        update();
+    }
+    QMainWindow::keyPressEvent(event);
+}
+
+void MenuWindow::onStartGameClicked()
+{
+    this->close();
+    GameWidget *game = new GameWidget();
+    game->setWindowTitle("I Wanna 游戏界面");
+    game->show();
+}
